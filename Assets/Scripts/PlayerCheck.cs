423c755a011f95b8f5efb88b8b4f322c53c0019a
JPMESRocks <joaopaulo1236@gmail.com>
2024-05-09 19:46:35 +0100

    Started work on room collider player check
    added Box Collider to Room1 prefab
    started writing assets/scripts/PlayerCheck.cs script

---
+++ Assets/Scripts/PlayerCheck.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project
+{
+    public class PlayerCheck : MonoBehaviour
+    {
+        public Collider boxcol;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
