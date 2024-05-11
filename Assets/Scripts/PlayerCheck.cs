9bfb453885379f6f2e708b687d68e05e665d0d58
JPMESRocks <joaopaulo1236@gmail.com>
2024-05-11 12:29:09 +0200

    Implemented colllider in Room1 prefab
    counts players in room

--- Assets/Scripts/PlayerCheck.cs
+++ Assets/Scripts/PlayerCheck.cs
@@ -6,17 +6,31 @@ namespace Project
 {
     public class PlayerCheck : MonoBehaviour
     {
-        public Collider boxcol;
-        // Start is called before the first frame update
-        void Start()
+        public List<GameObject> playersIn;
+        public int PlayersNeeded;
+
+
+        private void OnTriggerEnter(Collider other) 
         {
-            
+            GameObject otherobject = other.gameObject;
+            Debug.Log("Player entered room.");
+            if (otherobject.CompareTag("Player") && !playersIn.Contains(otherobject))
+            {
+                playersIn.Add(otherobject);
+            }
+            if (playersIn.Count == PlayersNeeded)
+            {
+                //Close room and start interactions
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnTriggerExit(Collider other) 
         {
-        
+            GameObject otherobject = other.gameObject;
+            if (otherobject.CompareTag("Player") )
+            {
+                playersIn.Remove(otherobject);
+            }
         }
     }
 }
