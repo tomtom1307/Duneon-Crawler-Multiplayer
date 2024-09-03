e5c5c6233c7f0e3d82fa4d0bb779bcc20df8d4fb
JPMESRocks <joaopaulo1236@gmail.com>
2024-09-03 18:23:37 +0200

    door triggers enemy spawn

--- Assets/BarrackDoorLogic.cs
+++ Assets/BarrackDoorLogic.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
+using System.Linq;
 
 namespace Project
 {
     public class BarrackDoorLogic : _Interactable
     {
+        [SerializeField] EnemySpawner roomSpawner;
         Animator anim;
         
         void Start()
@@ -18,7 +21,14 @@ namespace Project
         {
             base.Interact();
             anim.SetBool("Open", true);
+            BarrackSpawnServerRpc();
             Destroy(this);
+        }
+
+        [ServerRpc(RequireOwnership=false)]
+        void BarrackSpawnServerRpc()
+        {
+            roomSpawner.Active = true;
         }
     }
 }
