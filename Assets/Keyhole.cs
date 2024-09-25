afc80ce29b81b18ddba4c90460fca1e6d7729fa7
JPMESRocks <joaopaulo1236@gmail.com>
2024-09-25 18:22:08 +0200

    Made keyhole light up on activating
    - made dimred and glowingred materials

--- Assets/Keyhole.cs
+++ Assets/Keyhole.cs
@@ -9,10 +9,19 @@ namespace Project
         public Animator barricadeAnimator;
         public bool active;
         public string correspondingBarricade;
+        GameObject keyObject;
+        Renderer crystalRend;
+        public Material dimRed;
+        public Material glowingRed;
+        Material[] holematerials;
         // Start is called before the first frame update
         void Start()
         {
             active = true;
+            keyObject = transform.GetChild(0).gameObject;
+            crystalRend = keyObject.GetComponent<Renderer>();
+            holematerials = transform.gameObject.GetComponent<Renderer>().materials;
+            holematerials[2] = glowingRed;
         }
 
         protected override void Interact()
@@ -21,10 +30,14 @@ namespace Project
             {
 
                 active=false;
-                barricadeAnimator.SetTrigger("OpenB" + correspondingBarricade);
                 Prompt = "";
-                //Change material of inset to glowing one
                 //play sound
+                keyObject.SetActive(true);
+                //Crystalrend.material.Lerp(dimRed, glowingRed,t);
+                crystalRend.material = glowingRed;
+                transform.gameObject.GetComponent<Renderer>().materials = holematerials; //LERP THIS AND THE CRYSTAL MATERIAL IN A COROUTINE
+                //https://discussions.unity.com/t/help-using-lerp-inside-of-a-coroutine/207869
+                barricadeAnimator.SetTrigger("OpenB" + correspondingBarricade);
             }
         }
     }
