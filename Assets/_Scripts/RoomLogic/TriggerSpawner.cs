f6737fd3ac5ddb51239a4dd5d4ad5983f1551163
tomtom1307 <tom_ryan1307@icloud.com>
2024-10-02 14:27:42 +0200

    Chaos heart room visual improvemnets

---
+++ Assets/_Scripts/RoomLogic/TriggerSpawner.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Project
+{
+    public class TriggerSpawner : NetworkBehaviour
+    {
+        public EnemySpawner spawner;
+        public GameObject Wall;
+
+
+
+
+        [ServerRpc(RequireOwnership = false)]
+        public void OnActivateServerRpc(int Channel)
+        {
+            
+        }
+
+
+        private void Update()
+        {
+
+            
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            TriggerServerRpc();
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void TriggerServerRpc()
+        {
+            print("Triggered");
+            //Put Condition for players inside run 
+            spawner.Active = true;
+            
+        }
+
+
+    }
+
+}
