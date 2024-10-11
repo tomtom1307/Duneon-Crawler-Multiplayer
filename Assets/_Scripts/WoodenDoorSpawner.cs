ec3032f4c12cd36742da293cf70912ea8eedcf8f
TBebbers <Bastiaanebbers@gmail.com>
2024-10-11 16:41:26 +0200

    Update BustiWonderLand
    Added Crobling (Formerly known as FaceHugger, P.S. the prefab is still called this) Also made navmesh and now started working on the spawning of the enemies

---
+++ Assets/_Scripts/WoodenDoorSpawner.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using System.Linq;
+
+namespace Project
+{
+    public class WoodenDoorSpawner : _Interactable
+    {
+        [SerializeField] EnemySpawner roomSpawner;
+        Animator anim;
+        public bool triggerspawn = true;
+        PlayerCheck playerCheck;
+        GenericSoundSource soundSource;
+
+        void Start()
+        {
+            soundSource = GetComponent<GenericSoundSource>();
+            anim = GetComponent<Animator>();
+            playerCheck = transform.parent.parent.parent.gameObject.GetComponent<PlayerCheck>();
+        }
+        protected override void Interact()
+        {
+            if (!anim.GetBool("Open"))
+            {
+                base.Interact();
+                anim.SetBool("Open", true);
+                DoorSoundClientRpc();
+                if (triggerspawn) { triggerspawn = false; BarrackSpawnServerRpc(); }
+                Prompt = "";
+                //Destroy(this);
+
+            }
+
+
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        void BarrackSpawnServerRpc()
+        {
+            if (!roomSpawner.Active)
+            {
+                TriggerSpawner();
+                roomSpawner.Active = true;
+            }
+        }
+        public void TriggerSpawner()
+        {
+            // Actions.SpawnerUpdate(roomSpawner, true);
+            roomSpawner.StartSpawner();
+        }
+        [ClientRpc]
+        void DoorSoundClientRpc()
+        {
+            soundSource.PlaySound(GenericSoundSource.GenSoundType.DoorOpen, 1);
+        }
+    }
+}
